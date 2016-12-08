a16056688a24fc4794a3d98a584259dd9b40b9e8
juliakent77 <juliakent@Julias-MacBook-Air.local>
2016-12-07 19:53:42 -0800

    Boxes changing colors now

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-var container =document.getElementByID('container')
+var container = document.getElementById('container');
 
-for (var 1 = 0; i < 100; i++) {
+// build an element and then add it after, so it is not on your page in the very start
+
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for(var i = 0; i < 300; i++) {
   var elem = document.createElement('div');
+  elem.classList.add('box');
+
+  var rndNum = getRandomInt(1, 4); 
+  console.log (rndNum);
+  if (rndNum === 1) {
+    elem.classList.add('red');
+  } else if (rndNum === 2) {
+    elem.classList.add('blue');
+  } else if (rndNum === 3) {
+    elem.classList.add('green');
+  } else {
+    elem.classList.add('yellow');
+  }
+
   container.appendChild(elem);
 }
 
-// build an element and then add it after, so it is not on your page in the very start
+
+
